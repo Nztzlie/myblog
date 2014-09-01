fafb6a320e19eb51d8d8b2884b07825ecf7de95b
Natalie Baer <ncbaer@gmail.com>
2014-09-01 10:45:37 -0400

    added routes for projects threejs and interests

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
     resources :comments
   end
 
-  resources :threejs 
+  get 'threejs' => 'threejs#index'
+
+  get 'interests' => 'welcome#interests'
+  get 'pretty' => 'welcome#pretty'
+  get 'projects' => 'welcome#projects'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
